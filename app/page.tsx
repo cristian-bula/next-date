79f047b600d887f4b434a6125ea7628f62b64cf9
DESKTOP-QO38A17\elauycb <cristian.bula.dev@gmail.com>
2025-05-10 20:27:43 -0500

    seguridad 2

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,20 @@ export default async function DatesPage() {
       ?.filter((date) => (date?.date || new Date()) < new Date())
       ?.sort((a, b) => (b.date?.getTime() || 0) - (a.date?.getTime() || 0));
 
-    if (!upcomingDates) return;
+    if (!upcomingDates)
+      return (
+        <div className="min-h-screen flex items-center justify-center bg-olive-50">
+          <div className="text-center bg-white p-6 rounded-lg shadow-md max-w-md">
+            {/* <p className="text-red-500 text-lg mb-4">
+        Sorryyy, no pude cargar las citas. Please, recarga la página.
+      </p> */}
+            <div className="flex justify-center mt-2 gap-4">
+              <LoginModal />
+              <SignupModal />
+            </div>
+          </div>
+        </div>
+      );
     return (
       <Suspense
         fallback={
