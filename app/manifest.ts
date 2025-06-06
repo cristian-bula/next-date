85b6d3828ba8779a22a8d5d83508b4417c47fe28
DESKTOP-QO38A17\elauycb <cristian.bula.dev@gmail.com>
2025-06-06 14:49:21 -0500

    change color manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,61 +2,61 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Nuestra Próxima Cita",
-    short_name: "Nuestra Próxima Cita",
+    name: "Next Date",
+    short_name: "Next Date",
     description: "Contador para nuestra próxima cita romántica",
     start_url: "/",
     display: "standalone",
-    background_color: "#7ca659",
-    theme_color: "#7ca659",
+    background_color: "#e2e8cf",
+    theme_color: "#e2e8cf",
     icons: [
       {
-        src: "/icon-48x48.png",
+        src: "icons/icon-48x48.png",
         sizes: "48x48",
         type: "image/png",
       },
       {
-        src: "/icon-72x72.png",
+        src: "icons/icon-72x72.png",
         sizes: "72x72",
         type: "image/png",
       },
       {
-        src: "/icon-96x96.png",
+        src: "icons/icon-96x96.png",
         sizes: "96x96",
         type: "image/png",
       },
       {
-        src: "/icon-128x128.png",
+        src: "icons/icon-128x128.png",
         sizes: "128x128",
         type: "image/png",
       },
       {
-        src: "/icon-144x144.png",
+        src: "icons/icon-144x144.png",
         sizes: "144x144",
         type: "image/png",
       },
       {
-        src: "/icon-152x152.png",
+        src: "icons/icon-152x152.png",
         sizes: "152x152",
         type: "image/png",
       },
       {
-        src: "/icon-192x192.png",
+        src: "icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-256x256.png",
+        src: "icons/icon-256x256.png",
         sizes: "256x256",
         type: "image/png",
       },
       {
-        src: "/icon-384x384.png",
+        src: "icons/icon-384x384.png",
         sizes: "384x384",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "icons/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
